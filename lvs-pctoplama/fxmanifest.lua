808654d34a61e7ad16580e7ca124a44b615121e9
DrQwerciak <77661984+DrQwerciaks@users.noreply.github.com>
2023-08-01 08:02:56 +0200

    Add files via upload

---
+++ lvs-pctoplama/fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+this_is_a_map 'yes'
+
+client_scripts {
+	'client.lua',
+	'config.lua'
+}
+
+server_scripts {
+	'@mysql-async/lib/MySQL.lua', 
+    'server.lua',
+	'config.lua'
+}
+
+-- BU SCRİPT LEVİS DEVELOPMENTS TARAFINDAN YAPILMIŞTIR 
+-- TEKNİK DESTEK İÇİN : https://discord.gg/uB2GnsjCmb , ! Onur#5962
